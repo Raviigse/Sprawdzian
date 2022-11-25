7f1ccab50524a59790eeac7cc497b34beb5d2943
Raviigse <97956081+Raviigse@users.noreply.github.com>
2022-11-25 13:38:57 +0100

    Add files via upload

---
+++ skrypt.js
@@ -0,0 +1,7 @@
+function obliczanie(){
+    let a=document.getElementById("liczba1").value;
+    let an=document.getElementById("liczba2").value;
+    let n=document.getElementById("liczba3").value;
+    wynik=(an-a)/(n-1);
+    document.getElementById("wynik").value="Różnica ciągu arytmetycznego wynosi : "+wynik;
+}
